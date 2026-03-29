29d0effd57324a629bb96fa309b5594a25d9b7a1
UNAINR <munain605@gmail.com>
2026-03-29 12:07:02 +0500

    fixed variables

--- app/(root)/case-studies/[slug]/page.tsx
+++ app/(root)/case-studies/[slug]/page.tsx
@@ -1,29 +1,24 @@
+
 import { CaseStudyDetailView } from '@/modules/case-studies/ui/views/case-study-detail-view'
 import { client } from '@/sanity/lib/client'
 import { CASE_STUDY_BY_SLUG_QUERY } from '@/sanity/lib/queries'
 import { notFound } from 'next/navigation'
 
+export const revalidate = 60
+
 interface PageProps {
   params: Promise<{ slug: string }>
 }
 
-export async function generateStaticParams() {
-  const slugs = await client.fetch(
-    `*[_type == "project" && defined(slug.current)]{ "slug": slug.current }`
-  )
-  return slugs.map((item: { slug: string }) => ({ slug: item.slug }))
-}
-
-
 const CaseStudyPage = async ({ params }: PageProps) => {
-  const { slug } = await params
-  const project = await client.fetch(CASE_STUDY_BY_SLUG_QUERY, {slug})
+  const { slug } = await params;
+  const project = await client.fetch(CASE_STUDY_BY_SLUG_QUERY, { slug })
 
-  if (!project) notFound()
+  if (!project) {
+    notFound()
+  }
 
   return <CaseStudyDetailView project={project} />
 }
 
 export default CaseStudyPage
-
-export const revalidate = 60
