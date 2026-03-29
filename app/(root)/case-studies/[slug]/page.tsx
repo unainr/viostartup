86027af35d4deae5742325c7271d117e879d702c
UNAINR <munain605@gmail.com>
2026-03-29 12:02:49 +0500

    case-study-page-fixed

--- app/(root)/case-studies/[slug]/page.tsx
+++ app/(root)/case-studies/[slug]/page.tsx
@@ -1,28 +1,25 @@
-
 import { CaseStudyDetailView } from '@/modules/case-studies/ui/views/case-study-detail-view'
 import { client } from '@/sanity/lib/client'
 import { CASE_STUDY_BY_SLUG_QUERY } from '@/sanity/lib/queries'
 import { notFound } from 'next/navigation'
 
-
 interface PageProps {
   params: Promise<{ slug: string }>
 }
 
-const CaseStudyPage = async ({ params }: PageProps) => {
-  const { slug } = await params;
-if(!slug) return notFound()
-  let project
-  try {
-   project = await client.fetch(CASE_STUDY_BY_SLUG_QUERY, { slug })
-  
-} catch (error) {
-   console.error("Sanity error:", error);
-    notFound();
+export async function generateStaticParams() {
+  const slugs = await client.fetch(
+    `*[_type == "project" && defined(slug.current)]{ "slug": slug.current }`
+  )
+  return slugs.map((item: { slug: string }) => ({ slug: item.slug }))
 }
-   if (!project) {
-    notFound();
-  }
+
+
+const CaseStudyPage = async ({ params }: PageProps) => {
+  const { slug } = await params
+  const project = await client.fetch(CASE_STUDY_BY_SLUG_QUERY, {slug})
+
+  if (!project) notFound()
 
   return <CaseStudyDetailView project={project} />
 }
